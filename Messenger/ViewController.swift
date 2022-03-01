ceed75758f25846dc62b1e2f7e66a0f9c1fb3801
Vitalii-Vishnyakov <vlad_hiden00135@mail.ru>
2022-03-01 20:16:43 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Виталий on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
